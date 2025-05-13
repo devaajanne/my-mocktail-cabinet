bb0656976cef04e8f457aed8065067169492b12e
devaajanne <devaajanne@gmail.com>
2025-05-13 16:52:38 +0300

    Create routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/LoginForm.tsx";
+import Home from "./components/Home.tsx";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+      <Router>
+        <Routes>
+          <Route path='/' element={<LoginForm />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </Router>
+  );
 }
 
-export default App
+export default App;
