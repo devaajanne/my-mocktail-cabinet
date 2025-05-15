d5abb5b515c522df219667d09c2208f125d1a091
devaajanne <devaajanne@gmail.com>
2025-05-15 19:22:19 +0300

    Update to prettier style

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -39,47 +39,47 @@ const LoginForm: React.FC = () => {
 
   return (
     <>
-    <Box maxWidth={400} mx='auto' mt={4} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-      <Typography variant='h5' gutterBottom>
-        {isLogin ? "Log in to My Mocktail Cabinet" : "Sign up to My Mocktail Cabinet"}
-      </Typography>
-    </Box>
-
-      <Box maxWidth={400} mx='auto' mt={2}>
-      <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <Stack spacing={2}>
-          <TextField
-            label='Email'
-            type='email'
-            fullWidth
-            {...register("email", { required: "Email is required" })}
-            error={!!errors.email}
-            helperText={errors.email?.message}
-          />
-
-          <TextField
-            label='Password'
-            type='password'
-            fullWidth
-            {...register("password", { required: "Password is required" })}
-            error={!!errors.password}
-            helperText={errors.password?.message}
-          />
-
-          <Button type='submit' variant='contained' color='primary' disabled={isSubmitting}>
-            {isSubmitting ? (isLogin ? "Logging in..." : "Signing up...") : isLogin ? "Login" : "Sign up"}
-          </Button>
-        </Stack>
-      </form>
+      <Box maxWidth={400} mx='auto' mt={4} sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <Typography variant='h5' gutterBottom>
+          {isLogin ? "Log in to My Mocktail Cabinet" : "Sign up to My Mocktail Cabinet"}
+        </Typography>
       </Box>
 
-      <Box mt={4} sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box maxWidth={400} mx='auto' mt={2}>
+        <form onSubmit={handleSubmit(onSubmit)} noValidate>
+          <Stack spacing={2}>
+            <TextField
+              label='Email'
+              type='email'
+              fullWidth
+              {...register("email", { required: "Email is required" })}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+
+            <TextField
+              label='Password'
+              type='password'
+              fullWidth
+              {...register("password", { required: "Password is required" })}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+            />
+
+            <Button type='submit' variant='contained' color='primary' disabled={isSubmitting}>
+              {isSubmitting ? (isLogin ? "Logging in..." : "Signing up...") : isLogin ? "Login" : "Sign up"}
+            </Button>
+          </Stack>
+        </form>
+      </Box>
+
+      <Box mt={4} sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
         <Typography>{isLogin ? "Don't have an account yet?" : "Already have an account?"}</Typography>
-        <Button variant='outlined' color='primary'onClick={() => setIsLogin(!isLogin)} sx={{ mt: 1 }}>
+        <Button variant='outlined' color='primary' onClick={() => setIsLogin(!isLogin)} sx={{ mt: 1 }}>
           {isLogin ? "Sign up" : "Log in"}
         </Button>
       </Box>
-      </>
+    </>
   );
 };
 
